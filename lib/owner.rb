8993670d5064d412acbaf3f3337cbf508adba880
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-21 23:26:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -77,8 +77,8 @@ class Owner
   def sell_pets
     # self.cats.clear {|c| c.mood = "nervous"}
     # self.dogs.clear {|d| d.mood = "nervous"}
-    @@all_pets{:cats} << self.cats
-    @@all_pets{:dogs} << self.dogs
+    @@all_pets[:cats] << self.cats
+    @@all_pets[:dogs] << self.dogs
     
   
    end
